bc2d61b084d4393fa9a532e0916586578e2bf236
dionpapanik <dionysis_172@hotmail.com>
2015-11-02 00:34:18 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+if ($handle = opendir('./input')) {
+	$images = array();
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+			$images[] = $entry;
+            //print_r($images);
+        }
+    }
+    closedir($handle);
+}
+
+function compress_image($source_url, $destination_url, $quality) {
+	$info = getimagesize($source_url);
+	if ($info['mime'] == 'image/jpeg') $image = imagecreatefromjpeg($source_url);
+	elseif ($info['mime'] == 'image/gif') $image = imagecreatefromgif($source_url);
+	elseif ($info['mime'] == 'image/png') $image = imagecreatefrompng($source_url);
+	//save it
+	imagejpeg($image, $destination_url, $quality);
+	//return destination file url
+	return $destination_url;
+}
+foreach($images as $img){
+	$source_photo = 'input/'.$img;
+	$dest_photo = 'output/'.$img;
+	$d = compress_image($source_photo, $dest_photo, 70);
+	echo $img . " --> OK!</br>";
+}
+?>

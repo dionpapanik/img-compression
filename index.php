c8bcd59c9f5b2be7054afe595e79200e58adef4b
dionpapanik <dionysis_172@hotmail.com>
2015-11-02 00:43:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,15 +15,19 @@ function compress_image($source_url, $destination_url, $quality) {
 	if ($info['mime'] == 'image/jpeg') $image = imagecreatefromjpeg($source_url);
 	elseif ($info['mime'] == 'image/gif') $image = imagecreatefromgif($source_url);
 	elseif ($info['mime'] == 'image/png') $image = imagecreatefrompng($source_url);
-	//save it
-	imagejpeg($image, $destination_url, $quality);
-	//return destination file url
-	return $destination_url;
+	imagejpeg($image, $destination_url, $quality); //save it
+	return $destination_url; //return destination file url
 }
-foreach($images as $img){
-	$source_photo = 'input/'.$img;
-	$dest_photo = 'output/'.$img;
-	$d = compress_image($source_photo, $dest_photo, 70);
-	echo $img . " --> OK!</br>";
+
+if (empty($images)){
+	echo "dir empty!";
+	die;
+}else{
+	foreach($images as $img){
+		$source_photo = 'input/'.$img;
+		$dest_photo = 'output/'.$img;
+		$d = compress_image($source_photo, $dest_photo, 70);
+		echo $img . " --> OK!</br>";
+	}
 }
 ?>
